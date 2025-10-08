b042a801e3edce396e796b664cbe649111cf8838
AntonyRahul01 <antonyrahul2001@gmail.com>
2025-10-08 09:15:02 +0530

    ar

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const categoryController = require("../controllers/categoryController");
+const menuItemController = require("../controllers/menuItemController");
 const userAuth = require("../middlewares/userAuthMiddleware");
 
 // Register & OTP
 router.post("/register", userController.register);
 router.post("/verify-otp", userController.verifyOtp);
 router.post("/resend-otp", userController.resendOtp);
+router.get("/getCategories", categoryController.getCategories);
+router.get("/getMenuItems", menuItemController.getMenuItems);
 
 // Login
 router.post("/login", userController.login);
