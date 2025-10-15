903b7f48d5639ab6d23b47065e4d2edf2da4375c
portfoliodotmyspace <portfolio.myspace@gmail.com>
2025-10-15 13:19:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,33 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 // app.use(cors());
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+const allowedOrigins = [
+  "https://yogasfood.ch",
+  "http://localhost:5173",
+  "http://yogasfood.ch"
+];
+
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // if you plan to send cookies or auth headers
   })
 );
 app.use(bodyParser.json());
