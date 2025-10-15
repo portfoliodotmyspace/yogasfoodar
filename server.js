00f258101727c583468ccfc67af603d482ebeae8
portfoliodotmyspace <portfolio.myspace@gmail.com>
2025-10-15 08:15:03 +0530

    cors

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
